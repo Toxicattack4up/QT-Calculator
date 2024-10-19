2bee9d0d5d5775bc6a97d7abead7e7fedc070259
Леонид Кухарчик <kuxarchik007@mail.ru>
2024-10-19 20:24:58 +0300

    color changes have been made and code refactoring has been performed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,38 +5,60 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    QString a  =        "QPushButton { "
-               "border-radius: 25px; "
-               "background-color: #3b3b3b; "
-               "} "
-               "QPushButton:hover { "
-               "background-color: #003b3b; "
-               "} "
-               "QPushButton:pressed { "
-               "background-color: #1c598a; "
-                "}";
+    QString push_button  =
+            "QPushButton { "
+            "border-radius: 25px; "
+            "background-color: #3b3b3b; ""} "
+
+            "QPushButton:hover { "
+            "background-color: #545454; ""} "
+
+            "QPushButton:pressed { "
+            "background-color: #6e6e6e; ""}";
     ui->setupUi(this);
-    ui->button_1->setStyleSheet(a);
-    ui->button_2->setStyleSheet(a);
-    ui->button_3->setStyleSheet(a);
-    ui->button_4->setStyleSheet(a);
-    ui->button_5->setStyleSheet(a);
-    ui->button_6->setStyleSheet(a);
-    ui->button_7->setStyleSheet(a);
-    ui->button_8->setStyleSheet(a);
-    ui->button_9->setStyleSheet(a);
-    ui->button_0->setStyleSheet(a);
+    ui->button_1->setStyleSheet(push_button);
+    ui->button_2->setStyleSheet(push_button);
+    ui->button_3->setStyleSheet(push_button);
+    ui->button_4->setStyleSheet(push_button);
+    ui->button_5->setStyleSheet(push_button);
+    ui->button_6->setStyleSheet(push_button);
+    ui->button_7->setStyleSheet(push_button);
+    ui->button_8->setStyleSheet(push_button);
+    ui->button_9->setStyleSheet(push_button);
+    ui->button_0->setStyleSheet(push_button);
+    ui->button_plus_minus->setStyleSheet(push_button);
 
-    ui->button_plus->setStyleSheet(a);
-    ui->button_minus->setStyleSheet(a);
-    ui->button_equal->setStyleSheet(a);
-    ui->button_plus_minus->setStyleSheet(a);
-    ui->button_devide->setStyleSheet(a);
-    ui->button_multiply->setStyleSheet(a);
-    ui->button_erase_everything->setStyleSheet(a);
-    ui->button_one_character->setStyleSheet(a);
-    ui->button_delete_typed_text->setStyleSheet(a);
+    QString push_button_top  =
+        "QPushButton { "
+        "border-radius: 25px; "
+        "background-color: #717171; ""} "
 
+        "QPushButton:hover { "
+        "background-color: #8a8a8a; ""} "
+
+        "QPushButton:pressed { "
+        "background-color: #a3a3a3; ""}";
+
+    ui->button_one_character->setStyleSheet(push_button_top);
+    ui->button_erase_everything->setStyleSheet(push_button_top);
+    ui->button_delete_typed_text->setStyleSheet(push_button_top);
+
+    QString push_button_right  =
+        "QPushButton { "
+        "border-radius: 25px; "
+        "background-color: #ffad0a; ""} "
+
+        "QPushButton:hover { "
+        "background-color: #ffbe3d; ""} "
+
+        "QPushButton:pressed { "
+        "background-color: #ffcf70; ""}";
+
+    ui->button_plus->setStyleSheet(push_button_right);
+    ui->button_minus->setStyleSheet(push_button_right);
+    ui->button_equal->setStyleSheet(push_button_right);
+    ui->button_devide->setStyleSheet(push_button_right);
+    ui->button_multiply->setStyleSheet(push_button_right);
 }
 
 MainWindow::~MainWindow()
